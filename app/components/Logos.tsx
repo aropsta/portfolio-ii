aff99734d7b1284da44b98e22bf4229bcbb050ef
aropsta16 <aropsta16@gmail.com>
2024-10-02 08:56:00 +1000

    added tooltips to svg logos

--- app/components/Logos.tsx
+++ app/components/Logos.tsx
@@ -1,22 +1,82 @@
 "use client";
-import { Image, Flex } from "@chakra-ui/react";
-const items = [
-  "html.svg",
-  "css.svg",
-  "nextjs.svg",
-  "javascript.svg",
-  "typescript.svg",
-  "tailwind.svg",
-  "react.svg",
-  "cplus.svg",
-  "figma.svg",
-  "git.svg",
-  "java.svg",
-  "bash.svg",
-  "prisma.svg",
-  "react-query.svg",
-  "mysql.svg",
-  "axios.svg",
+import {
+  Image,
+  Flex,
+  Tooltip,
+  Box,
+  forwardRef,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { ForwardedRef } from "react";
+interface Item {
+  label: string;
+  file: string;
+}
+const items: Item[] = [
+  {
+    label: "HTML",
+    file: "html.svg",
+  },
+  {
+    label: "CSS",
+    file: "css.svg",
+  },
+  {
+    label: "Next.js",
+    file: "nextjs.svg",
+  },
+  {
+    label: "JavaScript",
+    file: "javascript.svg",
+  },
+  {
+    label: "TypeScript",
+    file: "typescript.svg",
+  },
+  {
+    label: "Tailwind CSS",
+    file: "tailwind.svg",
+  },
+  {
+    label: "React.js",
+    file: "react.svg",
+  },
+  {
+    label: "C++",
+    file: "cplus.svg",
+  },
+  {
+    label: "Figma",
+    file: "figma.svg",
+  },
+  {
+    label: "Git",
+    file: "git.svg",
+  },
+  {
+    label: "Java",
+    file: "java.svg",
+  },
+  {
+    label: "Bash",
+    file: "bash.svg",
+  },
+  {
+    label: "Prisma",
+    file: "prisma.svg",
+  },
+  {
+    label: "React Query",
+    file: "react-query.svg",
+  },
+  {
+    label: "MySQL",
+    file: "mysql.svg",
+  },
+  {
+    label: "Axios",
+    file: "axios.svg",
+  },
   // Add more logo paths here
 ];
 
@@ -24,13 +84,14 @@ const Tech = () => {
   return (
     <Flex wrap="wrap" gap="2" justifyContent="center">
       {items.map((item, index) => (
-        <Image
-          key={index}
-          height="3rem"
-          src={`/${item}`}
-          alt={item}
-          maxWidth="10rem"
-        />
+        <Tooltip label={item.label} hasArrow key={index}>
+          <Image
+            height="3rem"
+            src={item.file}
+            alt={item.label}
+            maxWidth="10rem"
+          />
+        </Tooltip>
       ))}
     </Flex>
   );
--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -2,19 +2,16 @@ import React from "react";
 import {
   Text,
   Image,
-  Divider,
   CardFooter,
   ButtonGroup,
-  Button,
   Card,
   CardBody,
-  Container,
-  Flex,
   Heading,
   Stack,
 } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/next-js";
 import { Project } from "../Projects";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 interface Props {
   project: Project;
@@ -23,35 +20,41 @@ interface Props {
 const ProjectCard = ({ project }: Props) => {
   return (
     <Card
-      maxW="sm"
+      maxW="md"
+      padding="4"
+      variant="elevated"
       _hover={{
         transform: "scale(1.05)",
-        transition: "all 0.2s ease",
+        transition: "all 0.24s ease",
       }}
     >
       <CardBody>
         <Image
           src={project.img}
           alt="Screenshot of website"
-          borderRadius="lg"
+          borderRadius="sm"
         />
         <Stack mt="6" spacing="3">
-          <Heading size="md">{project.title}</Heading>
-          <Text>{project.description}</Text>
+          <Heading fontSize="xl">{project.title}</Heading>
+          <Text color="#555">{project.description}</Text>
         </Stack>
       </CardBody>
       <CardFooter>
-        <ButtonGroup spacing="2">
-          <Button>
-            <Link href={project.git} variant="solid">
-              Github
-            </Link>
-          </Button>
-          <Button>
-            <Link href={project.site} variant="solid">
-              LinkedIn
-            </Link>
-          </Button>
+        <ButtonGroup spacing="4" fontWeight="600">
+          <Link
+            href={project.git}
+            variant="solid"
+            referrerPolicy="no-referrer"
+            color="primary.400"
+            verticalAlign="center"
+          >
+            Github
+            <ExternalLinkIcon mx="2px" textAlign="center" />
+          </Link>
+          <Link href={project.site} variant="solid" color="primary.400">
+            LinkedIn
+            <ExternalLinkIcon mx="2px" textAlign="center" />
+          </Link>
         </ButtonGroup>
       </CardFooter>
     </Card>
