dbfc5a8a8c9dd7200b11eac5244d7ee0ba074c3f
aropsta16 <aropsta16@gmail.com>
2024-10-09 14:53:11 +1100

    Change some colours and added icon in navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "@chakra-ui/next-js";
 import { useLayoutEffect, useRef, useState } from "react";
 import { SlMenu } from "react-icons/sl";
 import { GrClose } from "react-icons/gr";
-import { Text } from "@chakra-ui/react";
+import { Text, Image, HStack } from "@chakra-ui/react";
 const menu = [
   {
     title: "Projects",
@@ -60,6 +60,41 @@ const Navbar = () => {
     };
   }, []);
 
+  return (
+    <nav className="w-full shadow-[0_0_10px_rgba(0,0,0,0.09)] flex bg-white justify-between items-center z-50 px-6  sm:px-12 relative sm:py-4">
+      <Link
+        href="/"
+        padding="2"
+        variant="plain"
+        _hover={{ textDecoration: "none" }}
+      >
+        <HStack>
+          <Image
+            src="./favicon.ico"
+            width="32px"
+            objectFit="cover"
+            alt="logo"
+          />
+          <Text fontWeight="700" fontSize="xl">
+            arob.dev
+          </Text>
+        </HStack>
+      </Link>
+      <NavItems className="hidden h-full sm:flex left-[100%]" />
+
+      {navOpen && (
+        <NavItems className="flex flex-col sm:hidden fixed justify-center left-0 top-0 w-[76%] h-full bg-[#ecf0f3] ease-in duration-500" />
+      )}
+      <button
+        ref={btnRef}
+        onClick={toggleNav}
+        className="sm:hidden p-1.5 font-black cursor-pointer text-xl outline-2 outline-yellow-900 hover:bg-primary transition-colors duration-200 ease-in hover:text-white"
+      >
+        {!navOpen ? <SlMenu /> : <GrClose />}
+      </button>
+    </nav>
+  );
+
   function NavItems({ className }: { className: string }) {
     return (
       <ul
@@ -84,33 +119,6 @@ const Navbar = () => {
       </ul>
     );
   }
-
-  return (
-    <nav className="w-full shadow-[0_0_10px_rgba(0,0,0,0.09)] flex bg-white justify-between items-center z-50 px-6  sm:px-12 relative sm:py-4">
-      <Link
-        href="/"
-        padding="2"
-        variant="plain"
-        _hover={{ textDecoration: "none" }}
-      >
-        <Text fontWeight="700" fontSize="xl">
-          arob.dev
-        </Text>
-      </Link>
-      <NavItems className="hidden h-full sm:flex left-[100%]" />
-
-      {navOpen && (
-        <NavItems className="flex flex-col sm:hidden fixed justify-center left-0 top-0 w-[76%] h-full bg-[#ecf0f3] ease-in duration-500" />
-      )}
-      <button
-        ref={btnRef}
-        onClick={toggleNav}
-        className="sm:hidden p-1.5 font-black cursor-pointer text-xl outline-2 outline-yellow-900 hover:bg-yellow-400 hover:text-white"
-      >
-        {!navOpen ? <SlMenu /> : <GrClose />}
-      </button>
-    </nav>
-  );
 };
 
 export default Navbar;
